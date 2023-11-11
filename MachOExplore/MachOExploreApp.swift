5d254c67f37db82632a18d401c135b40ade57c48
resse <zrl920825@hotmail.com>
2023-11-11 22:11:26 +0800

    Initial Commit

---
+++ MachOExplore/MachOExploreApp.swift
@@ -0,0 +1,13 @@
+// MachOExplore
+// Created by: resse
+
+import SwiftUI
+
+@main
+struct MachOExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
